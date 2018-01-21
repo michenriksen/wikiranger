3013116092bad71fa5024047f8b4190bf23b4690
Michael Henriksen <michenriksen@neomailbox.ch>
2018-01-21 10:58:51 +0100

    Add filtering functionality to HTML report

--- lib/wikiranger/report.rb
+++ lib/wikiranger/report.rb
@@ -2,6 +2,14 @@ module Wikiranger
   class Report
     def initialize(contributions)
       @contributions = contributions.sort_by { |c| c.timestamp_object }.reverse
+      @contributors  = contributions.map(&:user).uniq.sort_by { |ip| ip.split(".").map(&:to_i) }
+      @pages         = contributions.map { |c| [c.pageid, c.title] }.uniq.sort_by { |p| p.last }
+      @contribs_per_contributor = @contributors.map do |c|
+        [c, contributions.count { |contrib| contrib.user == c }]
+      end.to_h
+      @contribs_per_page = @pages.map do |p|
+        [p.first, contributions.count { |contrib| contrib.pageid == p.first }]
+      end.to_h
     end
 
     def generate(destination)
